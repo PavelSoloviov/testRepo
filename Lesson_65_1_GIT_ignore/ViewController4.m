2a8d077477519201313224f4c048dabe337bc092
Pavel Soloviov <pvsol2013@gmail.com>
2017-11-08 20:54:27 +0200

    add VC4

---
+++ Lesson_65_1_GIT_ignore/ViewController4.m
@@ -0,0 +1,13 @@
+//
+//  ViewController4.m
+//  Lesson_65_1_GIT_ignore
+//
+//  Created by Pavel Soloviov on 08.11.17.
+//  Copyright © 2017 Pavel Soloviov. All rights reserved.
+//
+
+#import "ViewController4.h"
+
+@implementation ViewController4
+
+@end
