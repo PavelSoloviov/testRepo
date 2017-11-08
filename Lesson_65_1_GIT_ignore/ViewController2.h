4b09cf82e7c627dc54e3c01c238ffe01980503a1
Pavel Soloviov <pvsol2013@gmail.com>
2017-11-08 20:30:18 +0200

    add VC2

---
+++ Lesson_65_1_GIT_ignore/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  Lesson_65_1_GIT_ignore
+//
+//  Created by Pavel Soloviov on 08.11.17.
+//  Copyright © 2017 Pavel Soloviov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIView
+
+@end
