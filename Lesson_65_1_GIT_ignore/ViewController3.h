620df53b85b55bf0a7c426fa081ac10252f4adbe
Pavel Soloviov <pvsol2013@gmail.com>
2017-11-08 20:33:00 +0200

    add VC3

---
+++ Lesson_65_1_GIT_ignore/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  Lesson_65_1_GIT_ignore
+//
+//  Created by Pavel Soloviov on 08.11.17.
+//  Copyright © 2017 Pavel Soloviov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController3 : UIView
+
+@end
---
+++ Lesson_65_1_GIT_ignore/ViewController3.m
@@ -0,0 +1,21 @@
+//
+//  ViewController3.m
+//  Lesson_65_1_GIT_ignore
+//
+//  Created by Pavel Soloviov on 08.11.17.
+//  Copyright © 2017 Pavel Soloviov. All rights reserved.
+//
+
+#import "ViewController3.h"
+
+@implementation ViewController3
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
